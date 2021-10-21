483bbffb470807f601df6ea49a41d01611aeee0d
RocketMares <58720028+RocketMares@users.noreply.github.com>
2021-10-20 20:24:33 -0500

    cambio de sesion
    cambio de sesion

--- php/conexion.php
+++ php/conexion.php
@@ -7,9 +7,7 @@
         $BD_NAME = 'BovedaProyect';
         $USER = 'Analisis';
         $pass = 'Malitos20';
-        //$ServerName = "DESKTOP-4Q2P8VT";
-        //$ServerName = '99.85.26.227';
-        $ServerName = '99.85.24.8';
+        $ServerName = "DESKTOP-4Q2P8VT\SQLEXPRESS";
         $connectionInfo = ['Database' => "$BD_NAME", 'CharacterSet' => 'UTF-8', 'UID' => "$USER", 'PWD' => "$pass"];
         //Se prepara la conexi�n
         $con = sqlsrv_connect($ServerName, $connectionInfo);
